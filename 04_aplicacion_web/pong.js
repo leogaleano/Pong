437b2d716efbb150b1b86d25eac02e6fd4a1ce50
leonardo.galeano <leonardo.galeano@alumnos.uneatlantico.es>
2021-01-14 20:02:23 +0100

    funcionamiento raqueta y pelota y estilos del juego

--- 04_aplicacion_web/pong.js
+++ 04_aplicacion_web/pong.js
@@ -73,7 +73,114 @@ function Pelota(){
 	//coloca la pelota con las nuevas posiciones de x e y
 	ColocarElemento("pelota", posx,posy);
 }
+
+//Raqueta
+//se inicializan las direcciones de las raquetas en el eje Y
+var DirP1Y = 0;
+var DirP2Y = 0;
+//se establecen las posiciones de las raquetas en el eje X
+var PosP1X = 20+limXmin;
+var PosP2X = 560+limXmin;
+//se colocan las dos raquetas en el juego en la mitad de la pantalla para empezar la partida
+ColocarElemento("player1", PosP1X, 160);
+ColocarElemento("player2", PosP2X, 160);
+//se define la funcion de movimiento para las raquetas
+function mover(event){
+	//dependiendo de la tecla oprimida, cambiará la direccion de las raquetas, esto se identifica con el código de las teclas
+	var c = event.keyCode;
+	console.log(c);
+	switch(c){
+		//jugador 1
+		//arriba
+		//tecla w
+		case 87 :
+			DirP1Y = -1;
+			break;
+		//abajo	
+		//tecla s
+		case 83 :
+			DirP1Y = 1;
+			break;
+		//jugador 2
+		//arriba
+		//flecha arriba
+		case 38 :
+			DirP2Y = -1;
+			break;
+		//abajo
+		//flecha abajo
+		case 40 :
+			DirP2Y = 1;
+			break;
+	}
+}
+//Se define la funcion para que las raquetas sean capaces de detrenerse
+function parar(event){
+	var c = event.keyCode;
+	if(c == 87 || c == 83){
+		DirP1Y = 0;
+	}
+	if(c == 38 || c == 40){
+		DirP2Y = 0;
+	}
+}
+//se define la funcion que colocará y actualizara las raquetas durante la ejecución del programa
+function Raqueta(){
+	// se obtiene la posicion de la raqueta
+	var pos1 = obtenerPosY("player1");
+	//se actualiza la posicion segun la direccion que tenga
+	pos1+=DirP1Y;
+	//se definen los limites del tablero
+	if(pos1 <= limYmin){
+		ColocarElemento("player1", PosP1X, limYmin);
+	}
+	if(pos1 >= (limYmax-80)){
+		ColocarElemento("player1", PosP1X, (limYmax-80));
+	}
+	if(pos1 >= limYmin && pos1 <= (limYmax-80)){
+		ColocarElemento("player1", PosP1X, pos1);
+	}
+	// se obtiene la posicion de la raqueta
+	var pos2 = obtenerPosY("player2");
+	//se actualiza la posicion segun la direccion que tenga
+	pos2+=DirP2Y;
+	//se definen los limites del tablero
+	if(pos2 <= limYmin){
+		ColocarElemento("player2", PosP2X, limYmin);
+	}
+	if(pos2 >= (limYmax-80)){
+		ColocarElemento("player2", PosP2X, (limYmax-80));
+	}
+	if(pos2 >= limYmin && pos2 <= (limYmax-80)){
+		ColocarElemento("player2", PosP2X, pos2);
+	}
+	
+}
+//Choques
+function choque(){
+	//se crea el escenario del jugador 1 para cuando choque con la raqueta cambie de direccion
+	if(obtenerPosX("pelota") == (obtenerPosX("player1")+20)){
+		if(obtenerPosY("pelota") >= obtenerPosY("player1") && obtenerPosY("pelota") <= (obtenerPosY("player1")+80)){
+			movx = 1;
+		}
+	}
+	//se crea el escenario del jugador 2 para cuando choque con la raqueta cambie de direccion
+	if(obtenerPosX("pelota") == (obtenerPosX("player2")-20)){
+		if(obtenerPosY("pelota") >= obtenerPosY("player2") && obtenerPosY("pelota") <= (obtenerPosY("player2")+80)){
+			movx = -1;
+		}
+	}
+}
 //Funcion principal del juego
 function main(){
-	Pelota();
+    Pelota();
+	Raqueta();
+	choque();
+
+
 }
+
+
+setInterval("main();", velocidad);
+document.onkeydown = mover;
+document.onkeyup = parar;
